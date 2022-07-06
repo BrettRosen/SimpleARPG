3c12b34f612f007a6a972556451b90db9cf3402b
Brett Rosen <Brejuro23@gmail.com>
2022-07-06 14:35:53 -0400

    Initial Commit

---
+++ SimpleARPG/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SimpleARPG
+//
+//  Created by Brett Rosen on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleARPG/SimpleARPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleARPGApp.swift
+//  SimpleARPG
+//
+//  Created by Brett Rosen on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleARPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
